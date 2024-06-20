9d01e0ada2ed1fc4b0188d0047e8d380ca2021eb
Taliesin <taliesinstudy@gmail.com>
2024-06-20 20:02:29 +1000

    feat! blog router skeleton created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const { app } = require("./server.js");
+const { databaseConnect } = require("./utils/database.js");
 const PORT = process.env.PORT || 3000;
 
-
+// Connect to DB as soon as index.js runs, not recommeneded
+// databaseconnect();
 app.listen(PORT, () => {
     console.log("Server successfully started!");
+
+    // After server succesfully starts, connect to the database
+    databaseConnect();
 });
