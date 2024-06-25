ef06819ad0e151db66366995f8d16efd0809f762
Taliesin <taliesinstudy@gmail.com>
2024-06-25 16:03:12 +1000

    fix! fixing doc ref in models

--- src/models/BlogModel.js
+++ src/models/BlogModel.js
@@ -24,7 +24,8 @@ const blogSchema = new mongoose.Schema({
         required: true
     },
     author: {
-        type: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User",
         required: true
     },
     likes: {
